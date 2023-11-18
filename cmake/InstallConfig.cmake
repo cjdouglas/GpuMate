3e3fea98e23ee3b75975c0f09d9cf87826bb08bd
Chris Douglas <chris.douglas.210@gmail.com>
2023-11-17 22:51:23 -0600

    Finished v1 cmake installation

--- cmake/InstallConfig.cmake
+++
@@ -1,9 +0,0 @@
-find_package(hip REQUIRED)
-find_package(rocblas REQUIRED)
-  
-if (__GM_PLATFORM_NVIDIA__)
-  find_package(CUDAToolkit REQUIRED)
-endif()
-
-include("${CMAKE_CURRENT_LIST_DIR}/GpuMateTargets.cmake")
-
--- cmake/InstallTargets.cmake
+++ cmake/InstallTargets.cmake
@@ -1,25 +1,55 @@
+include(CMakePackageConfigHelpers)
+include(GNUInstallDirs)
+
+set(
+  GPU_MATE_INSTALL_LIBDIR
+  "${CMAKE_INSTALL_LIBDIR}/cmake/GpuMate"
+)
+
 install(
   TARGETS GpuMate
   EXPORT GpuMateTargets
-  LIBRARY DESTINATION lib
-  ARCHIVE DESTINATION lib
-  INCLUDES DESTINATION include
+  LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
 install(
   EXPORT GpuMateTargets
   FILE GpuMateTargets.cmake
   NAMESPACE GpuMate::
-  DESTINATION lib/cmake/GpuMate
+  DESTINATION ${GPU_MATE_INSTALL_LIBDIR}
 )
 
 install(
   DIRECTORY "${PROJECT_SOURCE_DIR}/GpuMate/include/gpu_mate"
-  DESTINATION include
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
+
+write_basic_package_version_file(
+  "GpuMateConfigVersion.cmake"
+  VERSION ${PROJECT_VERSION}
+  COMPATIBILITY SameMajorVersion
+)
+
+if (GM_VENDOR STREQUAL "AMD")
+  set(GM_BUILD_DEPENDENCIES "find_package(hip REQUIRED)\nfind_package(rocblas REQUIRED)")
+elseif (GM_VENDOR STREQUAL "NVIDIA")
+  set(GM_BUILD_DEPENDENCIES "find_package(CUDAToolkit REQUIRED)")
+endif()
+
+configure_file(
+  "${PROJECT_SOURCE_DIR}/cmake/InstallConfig.cmake.in" 
+  "${CMAKE_CURRENT_BINARY_DIR}/GpuMateConfig.cmake" 
+  @ONLY
 )
 
 install(
-  FILES "${PROJECT_SOURCE_DIR}/cmake/InstallConfig.cmake"
-  DESTINATION "lib/cmake/GpuMate"
-  RENAME "${PROJECT_NAME}Config.cmake"
+  FILES "${CMAKE_CURRENT_BINARY_DIR}/GpuMateConfig.cmake"
+  DESTINATION ${GPU_MATE_INSTALL_LIBDIR}
+)
+
+install(
+  FILES "${CMAKE_CURRENT_BINARY_DIR}/GpuMateConfigVersion.cmake"
+  DESTINATION ${GPU_MATE_INSTALL_LIBDIR}
 )
